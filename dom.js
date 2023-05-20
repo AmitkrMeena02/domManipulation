ac87a4f2ff72d059a45f3b074eef9d69e8e50e7a
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2023-05-20 14:17:53 +0530

    bold font & change background color

---
+++ dom.js
@@ -0,0 +1,16 @@
+//let headerTitle = document.getElementById(`header-title`);
+//let header = document.getElementById(`main-header`);
+//header.style.borderBottom = `solid 3px #000`;
+//let addTitle = document.querySelector(`.title`);
+//addTitle.style.fontWeight = `bold`;
+//addTitle.style.color = `green`;
+
+let items = document.getElementsByClassName(`list-group-item`);
+console.log(items);
+//change item background color
+items[2].style.backgroundColor = `green`;
+
+//each item change into bold;
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = `bold`;
+}
